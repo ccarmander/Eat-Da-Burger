493d073ec5d1fec67cec0b852131d191f7f0c48c
Chris Carman <ccarmander@gmail.com>
2020-11-24 11:11:01 -0500

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "root",
-    database: "burger_db"
+    user: "h3coiwo3770x56sg",
+    password: "syi8mvqh8aexw7n0",
+    database: "gdiqpu9ksqu5pmik"
   });
 }
 
